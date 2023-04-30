f4c85af43888c9c9fbd79da31510446266905505
Sandun Induranga <sandutharu40@gmail.com>
2023-04-30 21:27:41 +0530

    Header Page Updated

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,5 +1,16 @@
+import { Avatar } from "@mui/material";
+import logo from "../../assets/ijse.png";
+import userImage from "../../assets/avatar.jpg";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <header className="w-full h-20 bg-blue-600 fixed px-20 flex justify-between items-center">
+        <img src={logo} className="w-20" alt="" />
+        <Avatar src={userImage}></Avatar>
+      </header>
+    </>
+  );
 };
 
 export default Header;
