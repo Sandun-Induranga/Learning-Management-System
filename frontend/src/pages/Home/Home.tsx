179ec51f0801462d2e3c99cd4415a00618dca2fa
Sandun Induranga <sandutharu40@gmail.com>
2023-04-30 19:43:51 +0530

    Home Page Updated

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,5 +1,19 @@
-function Home() {
-  return <div>Home</div>;
-}
+import mainImage from "../../assets/main-page-image.jpg";
+
+const Home = () => {
+  return (
+    <>
+      <header className="w-full h-28 bg-black"></header>
+      <main className="w-full h-screen">
+        <section className="flex">
+          <div className="w-1/3 h-full">ONLINE LEARNING</div>
+          <div className="w-2/3 h-full">
+            <img src={mainImage} alt="" />
+          </div>
+        </section>
+      </main>
+    </>
+  );
+};
 
 export default Home;
