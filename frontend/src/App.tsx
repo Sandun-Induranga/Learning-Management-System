b3e51dddf5cb0f6ecec5e083420b22a0c55c49a2
Sandun Induranga <sandutharu40@gmail.com>
2023-04-30 20:28:22 +0530

    Login Page Created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <>
-      <Home />
+      {/* <Home /> */}
+      <Login />
     </>
   );
 }
---
+++ frontend/src/pages/Login/Login.tsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <>
+      <h1>ONLINE LEARNING</h1>
+    </>
+  );
+};
+
+export default Login;
