b938b3f9353049e2280aaeae7a9eae39bdd78cd5
Sandun Induranga <sandutharu40@gmail.com>
2023-04-30 18:53:16 +0530

    Home Page Created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl">Hello World</h1>
+      <Home />
     </>
   );
 }
