5fe8a1c1a726f4f25fd22da2d6ac4877176853b2
Sandun Induranga <sandutharu40@gmail.com>
2023-05-01 14:34:40 +0530

    Student Model Created

---
+++ backend/src/models/Student.ts
@@ -0,0 +1,28 @@
+import { Schema } from "mongoose";
+
+const StudentSchema = new Schema({
+  studentName: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  contact: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
