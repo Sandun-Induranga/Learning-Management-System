3a09c3381a8ee9aecdf4b13337afbdde3a7a303e
Sandun Induranga <sandutharu40@gmail.com>
2023-05-01 11:01:56 +0530

    Responsive Header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -8,9 +8,13 @@ import { useState } from "react";
 const Header = () => {
   const [isOpen, setOpen] = useState<boolean>(false);
 
+  const openCloseMenu = () => {
+    setOpen(!isOpen);
+  };
+
   return (
     <>
-      <header className="w-full h-20 fixed px-10 sm:px-20 flex justify-between items-center shadow top-0">
+      <header className="w-full h-20 fixed px-10 sm:px-20 flex justify-between items-center shadow top-0 z-50">
         <img src={logo} className="w-20" alt="" />
         <nav className="sm:flex gap-10 text-gray-700 hidden">
           <NavLink to={"/announcement"}>Announcement</NavLink>
@@ -22,9 +26,17 @@ const Header = () => {
         </div>
         <div className="sm:hidden">
           {!isOpen ? (
-            <Menu className="!text-gray-700" />
+            <Menu className="!text-gray-700" onClick={openCloseMenu} />
           ) : (
-            <Close className="!text-gray-700" />
+            <>
+              <Close className="!text-gray-700" onClick={openCloseMenu} />
+              <nav className="w-3/4 flex flex-col gap-10 absolute top-24 right-0 justify-center items-center bg-sky-edited-500 rounded-lg text-white p-10">
+                <NavLink to={"/announcement"}>Announcement</NavLink>
+                <NavLink to={"/announcement"}>Class Works</NavLink>
+                <NavLink to={"/announcement"}>Members</NavLink>
+                <Avatar className="border" src={userImage}></Avatar>
+              </nav>
+            </>
           )}
         </div>
       </header>
