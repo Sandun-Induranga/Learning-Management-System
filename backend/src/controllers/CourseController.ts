01ab37eee32955b2e606c5dd4d5c0ae49a3390e4
Sandun Induranga <sandutharu40@gmail.com>
2023-05-01 21:46:45 +0530

    Method saveCourse()

--- backend/src/controllers/CourseController.ts
+++ backend/src/controllers/CourseController.ts
@@ -1,1 +1,10 @@
-export default class CourseController {}
+import { Request, RequestHandler, Response } from "express";
+
+export default class CourseController {
+  saveCourse: RequestHandler = async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
+    return res.status(200).json({ message: "come" });
+  };
+}
