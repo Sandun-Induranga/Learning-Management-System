410d204c60c5027cfb45dfdebefac13e12f1646b
Sandun Induranga <sandutharu40@gmail.com>
2023-05-01 14:27:17 +0530

    Content Updated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,11 +9,10 @@ const app = express();
 db.connect(process.env.MONGO_DB_URL!)
   .then(() => {
     console.log("Db Connected");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server Listening of Port ${process.env.PORT}`);
+    });
   })
   .catch((error) => {
     console.log("Failed to Connect with MongoDb : " + error);
   });
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server Listening of Port ${process.env.PORT}`);
-});
