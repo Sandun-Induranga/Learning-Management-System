676dac604b48e29c83e68f5a7d81fb97d896b5da
Sandun Induranga <sandutharu40@gmail.com>
2023-05-01 10:40:43 +0530

    Responsive Header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,18 +2,24 @@ import { Avatar } from "@mui/material";
 import logo from "../../assets/ijse.png";
 import userImage from "../../assets/avatar.jpg";
 import { NavLink } from "react-router-dom";
+import { Menu } from "@mui/icons-material";
 
 const Header = () => {
   return (
     <>
-      <header className="w-full h-20 fixed px-20 flex justify-between items-center shadow top-0">
+      <header className="w-full h-20 fixed px-10 sm:px-20 flex justify-between items-center shadow top-0">
         <img src={logo} className="w-20" alt="" />
-        <nav className="flex gap-10 text-gray-700">
+        <nav className="sm:flex gap-10 text-gray-700 hidden">
           <NavLink to={"/announcement"}>Announcement</NavLink>
           <NavLink to={"/announcement"}>Class Works</NavLink>
           <NavLink to={"/announcement"}>Members</NavLink>
         </nav>
-        <Avatar className="border" src={userImage}></Avatar>
+        <div className="hidden sm:inline-block">
+          <Avatar className="border" src={userImage}></Avatar>
+        </div>
+        <div className="sm:hidden">
+          <Menu className="!text-gray-700" />
+        </div>
       </header>
     </>
   );
