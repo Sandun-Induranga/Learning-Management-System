b654773a9c20616e846bc720ef1695755772ea0f
Sandun Induranga <sandutharu40@gmail.com>
2023-05-03 02:02:57 +0530

    Api Testing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,3 @@
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Announcement from "./pages/Announcements";
-import ClassWorks from "./pages/ClassWorks/ClassWorks";
 import Content from "./components/Content";
 
 function App() {
--- frontend/src/pages/Announcements/Announcements.tsx
+++ frontend/src/pages/Announcements/Announcements.tsx
@@ -1,7 +1,38 @@
 import Header from "../../components/Header";
 import Announcement from "../../components/Announcement/Announcement";
+import api from "../../api";
+import { useEffect, useState } from "react";
+
+type StudentDetail = {
+  _id: string;
+  studentName: string;
+  address: string;
+  dob: Date;
+  email: string;
+  contact: string;
+  username: string;
+  password: string;
+  batchId: string;
+  profilePhoto: string;
+};
 
 const Announcements = () => {
+  const [studentList, setStudentList] = useState<StudentDetail[]>([]);
+
+  useEffect(() => {
+    getAllStudents();
+  }, []);
+
+  const getAllStudents = () => {
+    api
+      .get("student")
+      .then((res) => {
+        setStudentList(res.data.responesData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <>
       <Header />
@@ -9,6 +40,11 @@ const Announcements = () => {
         <Announcement />
         <Announcement />
         <Announcement />
+        <div>
+          {studentList.map((student) => (
+            <h1>{student._id}</h1>
+          ))}
+        </div>
       </div>
     </>
   );
