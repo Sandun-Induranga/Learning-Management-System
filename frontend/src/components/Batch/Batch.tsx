66c789553c7da8a9f065d870a21e164c52c33489
Sandun Induranga <sandutharu40@gmail.com>
2023-05-03 16:57:15 +0530

    Content Updated

--- frontend/src/components/Batch/Batch.tsx
+++ frontend/src/components/Batch/Batch.tsx
@@ -7,6 +7,9 @@ const Batch = (props: BatchDetail) => {
   return (
     <div className="border rounded-lg p-4 flex flex-col items-center text-2xl text-sky-edited-500 font-semibold cursor-pointer">
       {props.batchName}
+      <span className="text-xs text-gray-700 font-normal">
+        ID : {props._id}
+      </span>
     </div>
   );
 };
