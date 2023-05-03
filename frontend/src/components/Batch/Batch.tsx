b7cb31d8930ef2cab32782ae54f1508da27306e8
Sandun Induranga <sandutharu40@gmail.com>
2023-05-03 12:12:08 +0530

    Component Batch Updated

--- frontend/src/components/Batch/Batch.tsx
+++ frontend/src/components/Batch/Batch.tsx
@@ -1,7 +1,12 @@
-const Batch = () => {
+type BatchDetail = {
+  _id: string;
+  batchName: string;
+};
+
+const Batch = (props: BatchDetail) => {
   return (
     <div className="border rounded-lg p-4 flex flex-col items-center text-2xl text-sky-edited-500 font-semibold cursor-pointer">
-      GDSE 60
+      {props.batchName}
     </div>
   );
 };
--- frontend/src/pages/BatchPage/BatchPage.tsx
+++ frontend/src/pages/BatchPage/BatchPage.tsx
@@ -6,11 +6,11 @@ const BatchPage = () => {
     <>
       <Header />
       <main className="mt-20 p-10 grid sm:grid-cols-2 md:grid-cols-4 grid-cols-1 gap-4">
-        <Batch />
-        <Batch />
-        <Batch />
-        <Batch />
-        <Batch />
+        <Batch _id="1" batchName="GDSE 60" />
+        <Batch _id="1" batchName="GDSE 60" />
+        <Batch _id="1" batchName="GDSE 60" />
+        <Batch _id="1" batchName="GDSE 60" />
+        <Batch _id="1" batchName="GDSE 60" />
       </main>
     </>
   );
