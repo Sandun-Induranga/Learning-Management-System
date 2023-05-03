531b87346b6e6e11c020ba8e10f8fc41b40355f9
Sandun Induranga <sandutharu40@gmail.com>
2023-05-03 12:32:23 +0530

    Loaded Batches to Frontend

--- frontend/src/pages/BatchPage/BatchPage.tsx
+++ frontend/src/pages/BatchPage/BatchPage.tsx
@@ -1,16 +1,38 @@
 import Batch from "../../components/Batch/Batch";
 import Header from "../../components/Header/Header";
+import api from "../../api";
+import { useEffect, useState } from "react";
+
+type BatchDetail = {
+  _id: string;
+  batchName: string;
+};
 
 const BatchPage = () => {
+  const [batchList, setBatchList] = useState<BatchDetail[]>([]);
+
+  useEffect(() => {
+    getAllBatches();
+  });
+
+  const getAllBatches = () => {
+    api
+      .get("batch")
+      .then((res) => {
+        setBatchList(res.data.responseData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <>
       <Header />
       <main className="mt-20 p-10 grid sm:grid-cols-2 md:grid-cols-4 grid-cols-1 gap-4">
-        <Batch _id="1" batchName="GDSE 60" />
-        <Batch _id="1" batchName="GDSE 60" />
-        <Batch _id="1" batchName="GDSE 60" />
-        <Batch _id="1" batchName="GDSE 60" />
-        <Batch _id="1" batchName="GDSE 60" />
+        {batchList.map((batch) => (
+          <Batch key={batch._id} _id={batch._id} batchName={batch.batchName} />
+        ))}
       </main>
     </>
   );
