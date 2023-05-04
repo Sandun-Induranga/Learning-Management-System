8f63788453552ac0b2a3b02666d33d0ae1231c6e
Sandun Induranga <sandutharu40@gmail.com>
2023-05-04 12:55:44 +0530

    Content Updated

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -6,19 +6,22 @@ export interface IUser extends Document {
   role: string;
 }
 
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 export const User = model("User", UserSchema);
