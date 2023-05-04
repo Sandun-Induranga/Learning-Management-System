3b096d01059dfa4b85521b4c328e00e3020f31d7
Sandun Induranga <sandutharu40@gmail.com>
2023-05-04 08:36:04 +0530

    Student Form

--- frontend/src/pages/StudentPage/StudentPage.tsx
+++ frontend/src/pages/StudentPage/StudentPage.tsx
@@ -1,9 +1,83 @@
-import Header from "../../components/Header/Header";
+import { useState } from "react";
+import Header from "../../components/Header";
+import Student from "../../components/Student/Student";
+import { AddCircle, DoDisturbOn } from "@mui/icons-material";
+import { ThemeProvider } from "@emotion/react";
+import { Button, TextField, createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#007DFE",
+    },
+  },
+});
 
 const StudentPage = () => {
+  const [isClickedAddButton, setIsClickedAddButton] = useState<boolean>(false);
   return (
     <>
       <Header />
+      <section className="mt-20 p-10">
+        {!isClickedAddButton ? (
+          <>
+            <section
+              className="w-full h-12 rounded-t-lg bg-sky-edited-500 text-white flex justify-between items-center cursor-pointer px-20"
+              //   onClick={bindAddAndDiscartEvent}
+            >
+              <p>Add New Student</p>
+              <span>
+                <AddCircle />
+              </span>
+            </section>
+            <section
+              className="w-full h-32 border rounded-b-lg text-xl flex flex-col justify-center items-center text-gray-700 cursor-pointer"
+              //   onClick={bindAddAndDiscartEvent}
+            >
+              <AddCircle />
+              Add New Student
+            </section>
+          </>
+        ) : (
+          <>
+            <section
+              className="w-full h-12 rounded-t-lg bg-sky-edited-500 text-white flex justify-between items-center cursor-pointer px-20"
+              //   onClick={bindAddAndDiscartEvent}
+            >
+              <p>Discart Student</p>
+              <span>
+                <DoDisturbOn />
+              </span>
+            </section>
+            <section className="w-full border rounded-b-lg text-xl flex flex-col justify-center items-center text-gray-700 sm:p-10 p-5">
+              <form className="w-ful">
+                <ThemeProvider theme={theme}>
+                  <TextField
+                    label="Batch Name"
+                    fullWidth
+                    color="primary"
+                    name="batchName"
+                    // value={batchName}
+                    // onChange={handleInputChange}
+                    placeholder="Enter Batch Name"
+                    required
+                  />
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    fullWidth
+                    className="!mt-5"
+                  >
+                    Save Student
+                  </Button>
+                </ThemeProvider>
+              </form>
+            </section>
+          </>
+        )}
+
+        <main className="mt-10 grid sm:grid-cols-2 md:grid-cols-4 grid-cols-1 gap-4"></main>
+      </section>
     </>
   );
 };
