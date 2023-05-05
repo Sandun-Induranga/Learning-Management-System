12acfe17b947ef7f5efb9705c0a32a0fa9fe07c6
Sandun Induranga <sandutharu40@gmail.com>
2023-05-05 22:34:12 +0530

    Content Updated

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import BatchRoutes from "./BatchRoutes";
 import StudentRoutes from "./StudentRoutes";
 import AnnoucementRoutes from "./AnnouncementRoutes";
+import UserRoutes from "./UserRoutes";
 
 const router: Router = Router();
 
@@ -10,5 +11,6 @@ const url_prefix = "/api/v1/";
 router.use(`${url_prefix}batch`, new BatchRoutes().getRouter());
 router.use(`${url_prefix}student`, new StudentRoutes().getRouter());
 router.use(`${url_prefix}announcement`, new AnnoucementRoutes().getRouter());
+router.use(`${url_prefix}user`, new UserRoutes().getRouter());
 
 export default router;
