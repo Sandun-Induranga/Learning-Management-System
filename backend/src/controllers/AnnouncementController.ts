a92155019c06dc00439e3ab8152b2f434b7dfcac
Sandun Induranga <sandutharu40@gmail.com>
2023-05-05 10:23:26 +0530

    Save Announcement

--- backend/src/controllers/AnnouncementController.ts
+++ backend/src/controllers/AnnouncementController.ts
@@ -1,10 +1,23 @@
 import { Request, RequestHandler, Response } from "express";
+import { Announcement } from "../models/Announcement";
 
 export default class AnnouncementController {
   saveAnnouncement: RequestHandler = async (
     req: Request,
     res: Response
   ): Promise<Response> => {
-    return res;
+    try {
+      let announcement = new Announcement(req.body);
+      let savedAnnouncement = await announcement.save();
+      return res.status(200).json({
+        message: "Successfully Saved..!",
+        responseData: savedAnnouncement,
+      });
+    } catch (error: unknown) {
+      if (error instanceof Error)
+        return res.status(500).json({ message: error.message });
+
+      return res.status(500).json({ message: "Unknown Error Occured..!" });
+    }
   };
 }
