a938d95b8382660bf2a000a9cc52f35daa1356b8
Sandun Induranga <sandutharu40@gmail.com>
2023-05-05 12:15:41 +0530

    Get All Announcements

--- backend/src/controllers/AnnouncementController.ts
+++ backend/src/controllers/AnnouncementController.ts
@@ -20,4 +20,25 @@ export default class AnnouncementController {
       return res.status(500).json({ message: "Unknown Error Occured..!" });
     }
   };
+
+  getAllBatches: RequestHandler = async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
+    try {
+      let announcements = await Announcement.find();
+
+      return res
+        .status(200)
+        .json({
+          message: "Successfully Loaded..!",
+          responseData: announcements,
+        });
+    } catch (error: unknown) {
+      if (error instanceof Error)
+        return res.status(500).json({ message: error.message });
+
+      return res.status(500).json({ message: "Unknown Error Occured..!" });
+    }
+  };
 }
