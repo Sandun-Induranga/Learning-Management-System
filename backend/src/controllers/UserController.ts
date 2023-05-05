a07ba7e7da536ad636c0a81184d21aa7b0608515
Sandun Induranga <sandutharu40@gmail.com>
2023-05-05 22:22:52 +0530

    Login

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,1 +1,35 @@
-export default class UserController {}
+import { RequestHandler, Request, Response } from "express";
+import { User } from "../models/User";
+
+export default class UserController {
+  saveAnnouncement: RequestHandler = async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
+    try {
+      const { username, password } = req.body;
+      let user = await User.findOne({ username: username });
+      if (user) {
+        if (user.password == password) {
+          return res
+            .status(200)
+            .json({ message: "Login", responseData: user.role });
+        } else {
+          return res.status(200).json({
+            message: "Your Password is Wrong..!",
+            responseData: user.role,
+          });
+        }
+      } else {
+        return res
+          .status(200)
+          .json({ message: "Your Username is Wrong..!", responseData: "" });
+      }
+    } catch (error: unknown) {
+      if (error instanceof Error)
+        return res.status(500).json({ message: error.message });
+
+      return res.status(500).json({ message: "Unknown Error Occured..!" });
+    }
+  };
+}
