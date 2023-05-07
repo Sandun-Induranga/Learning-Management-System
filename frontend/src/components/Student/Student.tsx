5202d21189236f4ad2490e1f8dff6dc00c1b1496
Sandun Induranga <sandutharu40@gmail.com>
2023-05-07 22:14:15 +0530

    Student UI Model

--- frontend/src/components/Student/Student.tsx
+++ frontend/src/components/Student/Student.tsx
@@ -1,4 +1,12 @@
-import { Box, Button, Modal, TextField, ThemeProvider } from "@mui/material";
+import {
+  Box,
+  Button,
+  Modal,
+  TextField,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material";
+import React from "react";
 
 type StudentDetail = {
   _id: string;
@@ -13,10 +21,34 @@ type StudentDetail = {
   profilePhoto: string;
 };
 
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#007DFE",
+    },
+  },
+});
+
 const Student = (props: StudentDetail) => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <>
-      <tr className="h-12 cursor-pointer text-center">
+      <tr className="h-12 cursor-pointer text-center" onClick={handleOpen}>
         <td>{props._id}</td>
         <td>{props.studentName}</td>
         <td>{props.address}</td>
