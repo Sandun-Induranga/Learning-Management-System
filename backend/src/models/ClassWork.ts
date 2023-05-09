7812e9d119576327dc9b6a22486f03f9376210a6
Sandun Induranga <sandutharu40@gmail.com>
2023-05-09 15:26:13 +0530

    Update Batch

--- backend/src/models/ClassWork.ts
+++ backend/src/models/ClassWork.ts
@@ -1,18 +1,17 @@
 import { Document, Schema, model } from "mongoose";
 
 type Answer = {
-  studentId: string;
-  file: string[];
+  studentNic: string;
+  studentName: string;
+  submissionStatus: string;
+  file: string;
 };
 
 export interface IClassWork extends Document {
   type: string;
   dueDate: Date;
-  submissionStatus: string;
-  files: string[];
-  courseId: string;
-  moduleId: string;
-  answer: Answer[];
+  moduleName: string;
+  file: string;
 }
 
 const ClassWorkSchema = new Schema(
@@ -25,24 +24,12 @@ const ClassWorkSchema = new Schema(
       type: Date,
       required: true,
     },
-    submissionStatus: {
+    moduleName: {
       type: String,
       required: true,
     },
-    files: {
-      type: Array,
-      required: true,
-    },
-    courseId: {
+    file: {
       type: String,
-      required: true,
-    },
-    moduleId: {
-      type: String,
-      required: true,
-    },
-    answer: {
-      type: Array,
       required: true,
     },
   },
