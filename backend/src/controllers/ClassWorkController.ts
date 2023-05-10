61aaaa87a19ec7ef4d8d14f83fa648daa99be552
Sandun Induranga <sandutharu40@gmail.com>
2023-05-11 00:55:20 +0530

    Content Updated

--- backend/src/controllers/ClassWorkController.ts
+++ backend/src/controllers/ClassWorkController.ts
@@ -83,7 +83,7 @@ export default class ClassWorkController {
     }
   };
 
-  saveStudentImage: RequestHandler = async (
+  saveClassWorkImage: RequestHandler = async (
     req: Request,
     res: Response
   ): Promise<Response> => {
