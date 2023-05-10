d74d252ca5a332b62c010b87ec66054a8c6742e6
Sandun Induranga <sandutharu40@gmail.com>
2023-05-11 00:49:14 +0530

    Content Updated

--- backend/src/routes/ClassWorkRoutes.ts
+++ backend/src/routes/ClassWorkRoutes.ts
@@ -1,5 +1,6 @@
 import express, { Router } from "express";
 import ClassWorkController from "../controllers/ClassWorkController";
+import multer from "multer";
 
 export default class ClassWorkRoutes {
   private router: Router = express.Router();
@@ -10,10 +11,29 @@ export default class ClassWorkRoutes {
   }
 
   private configRoutes = (): void => {
+    const storage = multer.diskStorage({
+      destination: function (req, file, callback) {
+        callback(
+          null,
+          "/media/sandu/0559F5C021740317/GDSE/Project_Zone/VS_Projects/Learning-Management-System/frontend/public/uploads/classWorks"
+        );
+      },
+
+      filename: function (req, file, callback) {
+        callback(null, file.originalname);
+      },
+    });
+    const upload = multer({ storage: storage });
+
     this.router.post("/", this.classWorkController.saveClassWork);
     this.router.get("/", this.classWorkController.getAllClassworks);
     this.router.get("/:batch", this.classWorkController.getClassWorksByBatch);
     this.router.put("/:id", this.classWorkController.updateClassWork);
+    this.router.put(
+      "/image/:id",
+      upload.single("files"),
+      this.classWorkController.saveClassWork
+    );
   };
 
   public getRouter = (): Router => {
