8a2bd139d10cbb196a1f580e128d6aee305e618d
Sandun Induranga <sandutharu40@gmail.com>
2023-05-13 00:08:26 +0530

    Answer Model Created

--- backend/src/models/Answer.ts
+++ backend/src/models/Answer.ts
@@ -1,7 +1,37 @@
-type Answer = {
+import { Document, Schema, model } from "mongoose";
+
+export interface IAnswer extends Document {
   studentNic: string;
   studentName: string;
   submissionStatus: string;
   file: string;
   batch: string;
-};
+}
+
+const AnswerSchema = new Schema(
+  {
+    studentNic: {
+      type: String,
+      required: true,
+    },
+    studentName: {
+      type: String,
+      required: true,
+    },
+    submissionStatus: {
+      type: Date,
+      required: true,
+    },
+    file: {
+      type: String,
+      required: true,
+    },
+    batch: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Answer = model<IAnswer>("Answer", AnswerSchema);
