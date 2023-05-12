6be0a12f4bdbd0d85fa19bf571350c0440a7fbeb
Sandun Induranga <sandutharu40@gmail.com>
2023-05-12 20:27:50 +0530

    Updated Component Card

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -1,11 +1,16 @@
 import { Avatar } from "@mui/material";
 
-const Card = () => {
+type StudentProps = {
+  profilePhoto: string;
+  name: string;
+};
+
+const Card = (props: StudentProps) => {
   return (
     <>
       <aside className="text-lg">
-        <Avatar className="border" src={student.profilePhoto}></Avatar>
-        <h1>{student.studentName}</h1>
+        <Avatar className="border" src={props.profilePhoto}></Avatar>
+        <h1>{props.name}</h1>
       </aside>
     </>
   );
