b57f8dae2b4b1f1250c462f599da469fc266ada0
Sandun Induranga <sandutharu40@gmail.com>
2023-05-12 19:01:43 +0530

    Updated Members Page

--- frontend/src/components/Content/Content.tsx
+++ frontend/src/components/Content/Content.tsx
@@ -6,6 +6,7 @@ import ClassWorks from "../../pages/ClassWorks/ClassWorks";
 import BatchPage from "../../pages/BatchPage/BatchPage";
 import StudentPage from "../../pages/StudentPage/StudentPage";
 import ModulePage from "../../pages/ModulePage/ModulePage";
+import MembersPage from "../../pages/MembersPage/MembersPage";
 
 const Content = () => {
   return (
@@ -20,6 +21,7 @@ const Content = () => {
         <Route path="/teacher/students" element={<StudentPage />}></Route>
 
         <Route path="/modules" element={<ModulePage />}></Route>
+        <Route path="/student/members" element={<MembersPage />}></Route>
       </Routes>
     </>
   );
--- frontend/src/pages/MembersPage/MembersPage.tsx
+++ frontend/src/pages/MembersPage/MembersPage.tsx
@@ -1,5 +1,16 @@
+import Header from "../../components/Header";
+
 const MembersPage = () => {
-  return <div>MembersPage</div>;
+  return (
+    <>
+      <Header />
+      <section className="mt-20 p-10">
+        <h1 className="text-sky-edited-500 text-xl shadow-lg rounded-lg p-4">
+          Teachers
+        </h1>
+      </section>
+    </>
+  );
 };
 
 export default MembersPage;
