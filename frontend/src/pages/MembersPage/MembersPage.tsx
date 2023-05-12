a3af8d66e4c275ae5c154ca78c140594e4d5e26b
Sandun Induranga <sandutharu40@gmail.com>
2023-05-12 20:00:30 +0530

    Updated Members Page

--- frontend/src/pages/MembersPage/MembersPage.tsx
+++ frontend/src/pages/MembersPage/MembersPage.tsx
@@ -17,26 +17,60 @@ type StudentDetail = {
   updateStudentList: () => void;
 };
 
+type TeacherDetail = {
+  nic: string;
+  teacherName: string;
+  address: string;
+  email: string;
+  contact: string;
+  profilePhoto: string;
+  username: string;
+};
+
 const MembersPage = () => {
   const [studentList, setStudentList] = useState<StudentDetail[]>([]);
+  const [teacherList, setTeacherList] = useState<TeacherDetail[]>([]);
 
   useEffect(() => {
     loadAllStudents();
+    loadAllTeachers();
   }, []);
 
   const loadAllStudents = () => {
-    api.get(`student/${localStorage.getItem("currentBatch")}`).then((res) => {
-      setStudentList(res.data.responseData);
-    });
+    api
+      .get(`student/${localStorage.getItem("currentBatch")}`)
+      .then((res) => {
+        setStudentList(res.data.responseData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const loadAllTeachers = () => {
+    api
+      .get(`teacher`)
+      .then((res) => {
+        setTeacherList(res.data.responseData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
     <>
       <Header />
-      <section className="mt-20 p-10">
-        <h1 className="text-sky-edited-500 text-xl shadow-lg rounded-lg p-4">
-          Teachers
-        </h1>
+      <section className="mt-20 p-10 shadow-lg mx-10 rounded-lg">
+        <h1 className="text-sky-edited-500 text-xl p-4">Students</h1>
+        <aside className="grid grid-cols-4">
+          {teacherList.map((teacher) => (
+            <aside className="text-lg">
+              <Avatar className="border" src={teacher.profilePhoto}></Avatar>
+              <h1>{teacher.teacherName}</h1>
+            </aside>
+          ))}
+        </aside>
       </section>
 
       <section className="p-10 shadow-lg mx-10 rounded-lg">
