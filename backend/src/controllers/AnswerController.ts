2a385bbf180a8b1c22e8983ceed422c746dd7514
Sandun Induranga <sandutharu40@gmail.com>
2023-05-13 00:21:08 +0530

    Answer Controller Created

---
+++ backend/src/controllers/AnswerController.ts
@@ -0,0 +1,49 @@
+import { Request, RequestHandler, Response } from "express";
+import { Answer } from "../models/Answer";
+
+export default class AnswerController {
+  saveAnswer: RequestHandler = async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
+    try {
+      let answer = new Answer(req.body);
+      let savedAnswer = await answer.save();
+      return res.status(200).json({
+        message: "Successfully Saved..!",
+        responseData: savedAnswer,
+      });
+    } catch (error: unknown) {
+      if (error instanceof Error)
+        return res.status(500).json({ message: error.message });
+
+      return res.status(500).json({ message: "Unknown Error Occured..!" });
+    }
+  };
+
+  saveAnswerFile: RequestHandler = async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
+    try {
+      let { id } = req.params;
+
+      let answer = new Answer(await Answer.findById(id));
+      answer.file =
+        "../../../public/uploads/answers/" + req.file?.originalname.toString();
+
+      let updateAnswer = await Answer.findByIdAndUpdate(id, answer, {
+        new: true,
+      });
+
+      return res
+        .status(200)
+        .json({ message: "Uploaded", responseData: updateAnswer });
+    } catch (error: unknown) {
+      if (error instanceof Error)
+        return res.status(500).json({ message: error.message });
+
+      return res.status(500).json({ message: "Unknown Error Occured..!" });
+    }
+  };
+}
