c96a3dc88b8583c189e7d671d9aa1e0f508d909d
Sandun Induranga <sandutharu40@gmail.com>
2023-05-12 19:07:32 +0530

    Updated Members Page

--- frontend/src/pages/MembersPage/MembersPage.tsx
+++ frontend/src/pages/MembersPage/MembersPage.tsx
@@ -1,6 +1,30 @@
 import Header from "../../components/Header";
+import api from "../../api";
+import { useState } from "react";
+
+type StudentDetail = {
+  _id: string;
+  nic: string;
+  studentName: string;
+  address: string;
+  email: string;
+  contact: string;
+  username: string;
+  password: string;
+  batchName: string;
+  profilePhoto: string;
+  updateStudentList: () => void;
+};
 
 const MembersPage = () => {
+  const [studentList, setStudentList] = useState<StudentDetail[]>([]);
+
+  const loadAllStudents = () => {
+    api.get(`student/${localStorage.getItem("currentBatch")}`).then((res) => {
+      setStudentList(res.data.responseData);
+    });
+  };
+
   return (
     <>
       <Header />
