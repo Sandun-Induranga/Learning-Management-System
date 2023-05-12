f32e36421a2affffe5a76dbf8c21538e8cfcd011
Sandun Induranga <sandutharu40@gmail.com>
2023-05-12 19:50:56 +0530

    Updated Members Page

--- frontend/src/pages/MembersPage/MembersPage.tsx
+++ frontend/src/pages/MembersPage/MembersPage.tsx
@@ -1,6 +1,7 @@
 import Header from "../../components/Header";
 import api from "../../api";
 import { useEffect, useState } from "react";
+import { Avatar } from "@mui/material";
 
 type StudentDetail = {
   _id: string;
@@ -38,13 +39,12 @@ const MembersPage = () => {
         </h1>
       </section>
 
-      <section className="p-10">
-        <h1 className="text-sky-edited-500 text-xl shadow-lg rounded-lg p-4">
-          Students
-        </h1>
-        <aside className="grid grid-cols-5">
+      <section className="p-10 shadow-lg mx-10 rounded-lg">
+        <h1 className="text-sky-edited-500 text-xl p-4">Students</h1>
+        <aside className="grid grid-cols-4">
           {studentList.map((student) => (
-            <aside>
+            <aside className="text-lg">
+              <Avatar className="border" src={student.profilePhoto}></Avatar>
               <h1>{student.studentName}</h1>
             </aside>
           ))}
