37535c79b172e1d9a195063c8943cef7d48d01c3
Sandun Induranga <sandutharu40@gmail.com>
2023-05-13 20:19:46 +0530

    Added Answer Routes

---
+++ backend/src/routes/AnswerRoutes.ts
@@ -0,0 +1,39 @@
+import express, { Router } from "express";
+import AnswerController from "../controllers/AnswerController";
+import multer from "multer";
+
+export default class AnswerRoutes {
+  private router: Router = express.Router();
+  private answerController: AnswerController = new AnswerController();
+
+  constructor() {
+    this.configRoutes();
+  }
+
+  private configRoutes = (): void => {
+    const storage = multer.diskStorage({
+      destination: function (req, file, callback) {
+        callback(
+          null,
+          "/media/sandu/0559F5C021740317/GDSE/Project_Zone/VS_Projects/Learning-Management-System/frontend/public/uploads/classWorks"
+        );
+      },
+
+      filename: function (req, file, callback) {
+        callback(null, file.originalname);
+      },
+    });
+    const upload = multer({ storage: storage });
+
+    this.router.post("/", this.answerController.saveAnswer);
+    this.router.put(
+      "/image/:id",
+      upload.single("files"),
+      this.answerController.saveAnswerFile
+    );
+  };
+
+  public getRouter = (): Router => {
+    return this.router;
+  };
+}
