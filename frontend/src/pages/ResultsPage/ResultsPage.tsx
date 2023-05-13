60b5030faf0ddbb1567adb7adbe90b11ff54790d
Sandun Induranga <sandutharu40@gmail.com>
2023-05-13 23:05:06 +0530

    Content Updated

--- frontend/src/pages/ResultsPage/ResultsPage.tsx
+++ frontend/src/pages/ResultsPage/ResultsPage.tsx
@@ -1,5 +1,103 @@
+import { useState } from "react";
+import Header from "../../components/Header";
+import { AddCircle, DoDisturbOn } from "@mui/icons-material";
+import {
+  Button,
+  MenuItem,
+  Select,
+  TextField,
+  ThemeProvider,
+} from "@mui/material";
+
 const ResultsPage = () => {
-  return <div>ResultsPage</div>;
+  const [isClickedAddButton, setIsClickedAddButton] = useState<boolean>(true);
+  return (
+    <>
+      <Header />
+      <section className="mt-20 p-10">
+        {localStorage.getItem("currentRole") == "Teacher" ? (
+          <>
+            {!isClickedAddButton ? (
+              <>
+                <section
+                  className="w-full h-12 rounded-t-lg bg-sky-edited-500 text-white flex justify-between items-center cursor-pointer px-20"
+                  // onClick={bindAddAndDiscartEvent}
+                >
+                  <p>Add New Class Work</p>
+                  <span>
+                    <AddCircle />
+                  </span>
+                </section>
+                <section
+                  className="w-full h-32 border rounded-b-lg text-xl flex flex-col justify-center items-center text-gray-700 cursor-pointer"
+                  // onClick={bindAddAndDiscartEvent}
+                >
+                  <AddCircle />
+                  Add New Class Work
+                </section>
+              </>
+            ) : (
+              <>
+                <section
+                  className="w-full h-12 rounded-t-lg bg-sky-edited-500 text-white flex justify-between items-center cursor-pointer px-20"
+                  // onClick={bindAddAndDiscartEvent}
+                >
+                  <p>Discart Class Work</p>
+                  <span>
+                    <DoDisturbOn />
+                  </span>
+                </section>
+                <section className="w-full border rounded-b-lg text-xl flex flex-col justify-center items-center text-gray-700 sm:p-10 p-5">
+                  <form
+                    className="w-full flex flex-col gap-4"
+                    // onSubmit={handleSubmit}
+                  >
+                    <ThemeProvider theme={theme}>
+                      <TextField
+                        label="Name"
+                        fullWidth
+                        color="primary"
+                        name="classWorkName"
+                        // value={classWorkName}
+                        // onChange={handleInputChange}
+                        placeholder="Enter Class Work Name"
+                        required
+                      />
+                      <Select
+                        fullWidth
+                        // value={type}
+                        // onChange={handleTypeComboBox}
+                      >
+                        <MenuItem selected value={"Assignment"}>
+                          Assignment
+                        </MenuItem>
+                        <MenuItem value={"Project"}>Project</MenuItem>
+                        <MenuItem value={"Quize"}>Quize</MenuItem>
+                      </Select>
+
+                      <Button
+                        type="submit"
+                        variant="contained"
+                        fullWidth
+                        className="!mt-5"
+                      >
+                        Save Class Work
+                      </Button>
+                    </ThemeProvider>
+                  </form>
+                </section>
+              </>
+            )}
+          </>
+        ) : (
+          <></>
+        )}
+        <h1>Assignments</h1>
+        <h1>Courseworks</h1>
+        <h1>Projects</h1>
+      </section>
+    </>
+  );
 };
 
 export default ResultsPage;
