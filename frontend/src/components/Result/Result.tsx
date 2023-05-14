ca4802b44cd4f2d2e395ea591426e0dd655d4144
Sandun Induranga <sandutharu40@gmail.com>
2023-05-14 20:25:46 +0530

    Updated Component Result

--- frontend/src/components/Result/Result.tsx
+++ frontend/src/components/Result/Result.tsx
@@ -1,5 +1,19 @@
-const Result = () => {
-  return <div>Result</div>;
+type ResultProps = {
+  name: string;
+  moduleName: string;
+  mark: number;
+  grade: string;
+};
+
+const Result = (props: ResultProps) => {
+  return (
+    <div>
+      <h1>ClassWork : {props.name}</h1>
+      <h1>Module Name : {props.moduleName}</h1>
+      <h1>Marks : {props.mark}</h1>
+      <h1>Grade : {props.grade}</h1>
+    </div>
+  );
 };
 
 export default Result;
