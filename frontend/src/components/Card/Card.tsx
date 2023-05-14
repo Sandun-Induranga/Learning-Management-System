7f34683515587dc48c5438f50fa36a847e28fd70
Sandun Induranga <sandutharu40@gmail.com>
2023-05-14 15:57:48 +0530

    Content Updated

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -45,7 +45,84 @@ const Card = (props: StudentProps) => {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}></Box>
+        <Box sx={style}>
+          <section className="w-full border rounded-b-lg text-xl flex flex-col justify-center items-center text-gray-700 sm:p-10 p-5">
+            <form
+              className="w-full flex flex-col gap-4 sm:px-28"
+              onSubmit={handleSubmit}
+            >
+              <ThemeProvider theme={theme}>
+                <Select
+                  fullWidth
+                  value={module}
+                  onChange={handleModuleComboBox}
+                >
+                  <MenuItem disabled value={"Choose"}>
+                    Choose the Module
+                  </MenuItem>
+                  {moduleList.map((module) => (
+                    <MenuItem value={module.moduleName}>
+                      {module.moduleName}
+                    </MenuItem>
+                  ))}
+                </Select>
+                <Select
+                  fullWidth
+                  value={classWorkType}
+                  onChange={handleTypeComboBox}
+                >
+                  <MenuItem selected value={"Assignment"}>
+                    Assignment
+                  </MenuItem>
+                  <MenuItem value={"Project"}>Project</MenuItem>
+                  <MenuItem value={"Quize"}>Quize</MenuItem>
+                </Select>
+                <Select
+                  fullWidth
+                  value={classWorkName}
+                  onChange={handleNameComboBox}
+                >
+                  <MenuItem disabled value={"Choose Name"}>
+                    Choose Name
+                  </MenuItem>
+                  {classWorkList.map((work) => (
+                    <MenuItem value={work._id}>{work.name}</MenuItem>
+                  ))}
+                </Select>
+                <TextField
+                  label="Marks"
+                  fullWidth
+                  color="primary"
+                  name="marks"
+                  value={marks}
+                  onChange={handleInputChange}
+                  placeholder="Enter Marks"
+                  required
+                />
+
+                <TextField
+                  label="Grade"
+                  fullWidth
+                  color="primary"
+                  name="grade"
+                  value={grade}
+                  onChange={handleInputChange}
+                  placeholder="Enter Marks"
+                  required
+                />
+
+                <Button
+                  type="submit"
+                  variant="contained"
+                  fullWidth
+                  className="!mt-5"
+                >
+                  Save Class Work
+                </Button>
+              </ThemeProvider>
+            </form>
+          </section>
+        </Box>
       </Modal>
     </>
   );
