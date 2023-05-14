5c8db42585f6be3e788ce8f5e73329e8288409c6
Sandun Induranga <sandutharu40@gmail.com>
2023-05-14 16:19:01 +0530

    Created Result Routes

---
+++ backend/src/routes/ResultRoutes.ts
@@ -0,0 +1,19 @@
+import express, { Router } from "express";
+import ResultController from "../controllers/ResultController";
+
+export default class BatchRoutes {
+  private router: Router = express.Router();
+  private resultController: ResultController = new ResultController();
+
+  constructor() {
+    this.configRoutes();
+  }
+
+  private configRoutes = (): void => {
+    this.router.post("/", this.resultController.saveResult);
+  };
+
+  public getRouter = (): Router => {
+    return this.router;
+  };
+}
