db4c4cdc3dce0fd83e8e24ae8536ba93ef906359
Sandun Induranga <sandutharu40@gmail.com>
2023-05-14 20:00:30 +0530

    Content Updated

--- frontend/src/pages/ResultsPage/ResultsPage.tsx
+++ frontend/src/pages/ResultsPage/ResultsPage.tsx
@@ -9,8 +9,20 @@ type ResultDetail = {
   grade: string;
 };
 
+type ClassWorkDetail = {
+  _id: string;
+  name: string;
+  description: string;
+  type: string;
+  dueDate: string;
+  moduleName: string;
+  file: string;
+  batch: string;
+};
+
 const ResultsPage = () => {
   const [resultList, setResultList] = useState<ResultDetail[]>([]);
+  const [classWork, setClassWork] = useState<ClassWorkDetail>();
 
   useEffect(() => {
     getResults();
@@ -22,11 +34,25 @@ const ResultsPage = () => {
     });
   };
 
+  const getClassWorkById = (classWorkId: string) => {
+    api.get(`classwork/${classWorkId}`).then((res) => {
+      setClassWork(res.data.responseData);
+    });
+  };
+
   return (
     <>
       <Header />
       <section className="mt-20 p-10 sm:px-60">
         <h1>Assignments</h1>
+        {resultList.map((result) => (
+          <>
+            {getClassWorkById(result.classWorkId)}
+            <h1>ClassWork : {classWork?.name}</h1>
+            <h1>Marks : {result.mark}</h1>
+            <h1>Grade : {result.grade}</h1>
+          </>
+        ))}
         <h1>Courseworks</h1>
         <h1>Projects</h1>
       </section>
