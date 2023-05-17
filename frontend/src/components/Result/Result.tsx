de3c7a59949b95fd319a988bad8bda4f6db619ed
Sandun Induranga <sandutharu40@gmail.com>
2023-05-17 16:31:17 +0530

    Content Updated

--- frontend/src/components/Result/Result.tsx
+++ frontend/src/components/Result/Result.tsx
@@ -7,11 +7,15 @@ type ResultProps = {
 
 const Result = (props: ResultProps) => {
   return (
-    <div className="flex flex-col">
-      <h1>ClassWork : {props.name}</h1>
-      <h1>Module Name : {props.moduleName}</h1>
-      <h1>Marks : {props.mark}</h1>
-      <h1>Grade : {props.grade}</h1>
+    <div className="flex flex-col justify-center items-center gap-2 p-10 border rounded-lg shadow-lg text-lg text-gray-700 w-1/2">
+      <div>
+        <h1>ClassWork : {props.name}</h1>
+        <h1>Module Name : {props.moduleName}</h1>
+        <h1>Marks : {props.mark}</h1>
+        <h1>
+          Grade : <span className="text-red-700">{props.grade}</span>
+        </h1>
+      </div>
     </div>
   );
 };
